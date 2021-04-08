e5eb1b0bf0e784a23ee53496f6346989e2bd4e86
Muhammad Imam Setiawan <imamsetiawan27062000@gmail.com>
2021-04-08 12:20:17 +0700

    BLoC State Management (flutter_bloc Package)

--- bloc_blocflutterpackage/lib/color_bloc.dart
+++ bloc_blocflutterpackage/lib/color_bloc.dart
@@ -1,17 +1,17 @@
-import 'package:bloc/bloc.dart';
+import 'dart:async';
 import 'package:flutter/material.dart';
+import 'package:bloc/bloc.dart';
 
-enum ColorEvent { to_amber, to_light_blue }
+enum ColorEvent { to_amber, to_blue }
 
 class ColorBloc extends Bloc<ColorEvent, Color> {
   Color _color = Colors.amber;
 
-  @override
-  Color get initialState => Colors.amber;
+  ColorBloc({Color initialState = Colors.amber}) : super(initialState);
 
   @override
   Stream<Color> mapEventToState(ColorEvent event) async* {
-    _color = (event == ColorEvent.to_amber) ? Colors.amber : Colors.lightBlue;
+    _color = (event == ColorEvent.to_amber) ? Colors.amber : Colors.blue;
     yield _color;
   }
 }
